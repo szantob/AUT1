11b518aa593d59923f4f535ec32525bd301dee8f
szantob <szantobenedek@gmail.com>
2019-10-07 15:45:57 +0200

    Task 5 start

--- src/main/java/hu/bme/aut/retelab2/repository/AdRepository.java
+++ src/main/java/hu/bme/aut/retelab2/repository/AdRepository.java
@@ -5,16 +5,26 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import hu.bme.aut.retelab2.domain.Ad;
+import hu.bme.aut.retelab2.domain.Subscription;
 
 @Repository
 public class AdRepository {
 
+	@Autowired
+	private SubscriptionRepository subRepo;
+	
     @PersistenceContext
     private EntityManager em;
+    
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     @Transactional
     public Ad save(Ad ad) {
@@ -40,6 +50,16 @@ public class AdRepository {
         Ad oldAd = em.find(Ad.class, newAd.getId());
         if(oldAd.getSecret().equals(newAd.getSecret())) {
         	em.merge(newAd);
+        	List<Subscription> subList = subRepo.getSubscriptionsByAd(newAd);
+        	for (Subscription subscription : subList) {
+        		SimpleMailMessage msg = new SimpleMailMessage();
+        		msg.setFrom("username@gmail.com");
+        		msg.setTo(subscription.getEmail());
+        		msg.setSubject(newAd.getTitle());
+        		msg.setText(newAd.getDesc());
+        		javaMailSender.send(msg);
+
+			}
         	return newAd;
         }
         return null;
--- src/main/java/hu/bme/aut/retelab2/repository/SubscriptionRepository.java
+++ src/main/java/hu/bme/aut/retelab2/repository/SubscriptionRepository.java
@@ -1,11 +1,14 @@
 package hu.bme.aut.retelab2.repository;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import hu.bme.aut.retelab2.domain.Ad;
 import hu.bme.aut.retelab2.domain.Subscription;
 
 @Repository
@@ -17,5 +20,14 @@ public class SubscriptionRepository {
     @Transactional
     public Subscription save(Subscription sc) {
         return em.merge(sc);
+    }
+    
+    @Transactional
+    public List<Subscription> getSubscriptionsByAd(Ad ad) {
+    	String query = "SELECT a FROM Subscription a WHERE Ad == ?1;";
+		return em.createQuery(query, Subscription.class)
+        		.setParameter(1, ad)
+        		.getResultList();
+    	
     }
 }
